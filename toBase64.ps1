cfbed2103451f8b105c15dc29ab8f27be9ccc463
leoyulee <leoyulee.585@gmail.com>
2026-03-01 12:11:42 -0600

    Utility - Convert CRLF to LF for more compact base64 outputs

--- toBase64.ps1
+++ toBase64.ps1
@@ -1,9 +1,13 @@
 # Define the original string and the output file path
 $OriginalString = Get-Content -Path $args[0] -Raw
+
+#Convert CRLF to LF
+$StringToConvert = $OriginalString -replace "`r`n", "`n"
+
 $OutputPath = $args[1] # Use an absolute path
 
 # 1. Convert the string to a byte array using the appropriate encoding (e.g., UTF-8 is efficient)
-$Bytes = [System.Text.Encoding]::UTF8.GetBytes($OriginalString)
+$Bytes = [System.Text.Encoding]::UTF8.GetBytes($StringToConvert)
 
 # 2. Convert the byte array to a standard Base64 string
 $Base64String = [Convert]::ToBase64String($Bytes)
